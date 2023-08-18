5dd67c5671ddeecc3b30750f6341c5be0725fa6a
RajRonal <108928775+RajRonal@users.noreply.github.com>
2023-08-18 14:57:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and Ronal.
+          Edit <code>src/App.js</code> and Ronal. Raj
         </p>
         <a
           className="App-link"
